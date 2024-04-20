78a56b4016cc5777a7f417bf7146b667b18fa95e
Ritik <ritikraushan160@gmail.com>
2024-04-20 16:20:45 +0530

    commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ function Footer() {
       <div className="flex absolute inset-y-0 left-0 items-center pointer-events-none">
         <svg className="w-5 h-5 text-purple-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
       </div>
-      <input className="block p-2 pl-10 w-full text-sm text-gray-900 rounded-xl border border-slate-500 sm:rounded-none sm:rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:purple-500 dark:placeholder-black-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="  Enter your email" type="email" id="email" required=""/>
+      <input className="block p-2 pl-10 w-full text-sm text-gray-900 rounded-xl border border-slate-500  sm:rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:purple-500 dark:placeholder-black-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="  Enter your email" type="email" id="email" required=""/>
     </div>
     <div className='ml-2'>
       <button type="submit" className="py-2 w-24 text-sm font-semibold text-center text-blaCK rounded-lg border cursor-pointer bg-purple-500 border-purple-500 sm:rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 hover:bg-purple-900">Subscribe</button>
