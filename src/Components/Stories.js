c5507d787d46c12aa0a26be8097208929ff51d45
Ritik <ritikraushan160@gmail.com>
2024-04-20 21:01:26 +0530

    commit

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -6,9 +6,9 @@ function Stories({details}) {
 
 
   return (
-    <div class="relative flex flex-wrap flex-row mx-auto mt-10 cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-90  duration-300">
+    <div class="relative flex flex-wrap flex-col md:flex-row  mx-auto mt-10 cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-90  duration-300">
       <div className='m-5'>
-        <a href={details.story} target='new'><img class="h-60 w-40 object-cover rounded-md" src={details.url} alt="Random image"/></a>
+        <a href={details.story} target='new'><img class="h-30 w-20 md:h-60 md:w-40 object-cover rounded-md" src={details.url} alt="Random image"/></a>
         </div>
     </div>
   )
